1c35a62ef20f10073f5db43d6afa5e092aca8f75
ou <vremenno@bk.ru>
2025-10-28 17:20:40 +0300

    2ое задание начало подзадач

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,42 +1,66 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import os
+import json
 
 app = FastAPI()
-tasks = [{'id': 1, 'task': 'Закончить простые бэки', 'status': 'No'}, {'id': 2, 'task': 'Хз', 'status': 'Yes'}]
+
+json_name = 'tasks.json'
+
+def load_tasks():
+    if not os.path.exists(json_name):
+        return []
+    
+    try:
+        with open(json_name, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+
+def save_tasks(task_list):
+    with open(json_name, 'w', encoding='utf-8') as file:
+        json.dump(task_list, file)
 
 class CreateTask(BaseModel):
     task: str
     status: str = 'No'
+    
 class UpdateTask(BaseModel):
     task: str
     status: str
 
 @app.get("/tasks", tags=['Вывод всех задач'])
 def get_tasks():
-    return tasks
+    return load_tasks()
 
 @app.post("/tasks", tags=['Добавление задачи'])
 def create_task(new_task: CreateTask):
-    max_id = max(task['id'] for task in tasks)
-    tasks.append({'id': max_id + 1, 'task': new_task.task, 'status': new_task.status})
+    tasks = load_tasks()
+    max_id = max(task['id'] for task in tasks) + 1
+    tasks.append({'id': max_id, 'task': new_task.task, 'status': new_task.status})
+    save_tasks(tasks)
     return {"success": True}
 
 @app.put("/tasks/{task_id}", tags=['Обновление задачи'])
 def update_task(task_id: int, update_task: UpdateTask):
+    tasks = load_tasks()
     for i in tasks:
         if i['id'] == task_id:
             i['task'] = update_task.task
             i['status'] = update_task.status
+            save_tasks(i)
             return {"success": True}
      
     raise HTTPException(status_code=404, detail = 'Задача не найдена')
 
 @app.delete("/tasks/{task_id}", tags=['Удаление задачи'])
 def delete_task(task_id: int):
-    global tasks
+    tasks = load_tasks()
     for i, task in enumerate(tasks):
         if task['id'] == task_id:
             tasks.pop(i)
+            save_tasks(tasks)
             return     {"success": True}     
         
     raise HTTPException(status_code=404, detail = 'Задача не найдена')
